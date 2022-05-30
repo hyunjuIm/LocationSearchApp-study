70044fd7def8e79a6e89ee00bfa7d5989923618f
hyunju <dear.jjwim@gmail.com>
2022-05-30 23:04:26 +0900

    Google Map api 연동

---
+++ app/src/main/java/com/example/locationsearchapp/response/address/AddressInfoResponse.kt
@@ -0,0 +1,5 @@
+package com.example.locationsearchapp.response.address
+
+data class AddressInfoResponse(
+    val addressInfo:AddressInfo
+)
--- app/src/main/java/com/example/locationsearchapp/utility/ApiService.kt
+++ app/src/main/java/com/example/locationsearchapp/utility/ApiService.kt
@@ -2,6 +2,7 @@ package com.example.locationsearchapp.utility
 
 import com.example.locationsearchapp.resource.Key
 import com.example.locationsearchapp.resource.Url
+import com.example.locationsearchapp.response.address.AddressInfoResponse
 import com.example.locationsearchapp.response.search.SearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -29,7 +30,6 @@ interface ApiService {
         @Query("centerLat") centerLat: String? = null
     ): Response<SearchResponse>
 
-    /*
     @GET(Url.GET_TMAP_REVERSE_GEO_CODE)
     suspend fun getReverseGeoCode(
         @Header("appKey") appKey: String = Key.TMAP_API,
@@ -40,5 +40,4 @@ interface ApiService {
         @Query("coordType") coordType: String? = null,
         @Query("addressType") addressType: String? = null
     ): Response<AddressInfoResponse>
-     */
 }
